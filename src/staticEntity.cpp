152ff51460153733a2a31584f41055407ae2eecd
Luca Morgado <luca.morgado.pro@gmail.com>
2023-12-12 19:05:56 +0100

    fixed static tile rendering problem

--- src/staticEntity.cpp
+++ src/staticEntity.cpp
@@ -5,6 +5,6 @@ void StaticEntity::render(Renderer *renderer)
 {
     // calculate the screenspace position
     Vector2f screenspacePos = renderer->worldspaceToScreenspace(pos);
-    SDL_Rect destRect = {(int)screenspacePos.x*SCALE_FACTOR, (int)screenspacePos.y*SCALE_FACTOR, width*SCALE_FACTOR, height*SCALE_FACTOR};
+    SDL_Rect destRect = {static_cast<int>(ceil(screenspacePos.x*SCALE_FACTOR)), static_cast<int>(ceil(screenspacePos.y*SCALE_FACTOR)), width*SCALE_FACTOR, height*SCALE_FACTOR};
     SDL_RenderCopy(renderer->getRenderer(), tex, NULL, &destRect);
 }
